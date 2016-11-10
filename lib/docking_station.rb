ec0f1b348c50dcbe486cefa3703dc656e4627949
Agata Sumowska <agata.sumowska@gmail.com>
2016-11-10 12:21:15 +0000

    Raise errors when trying to get more bikes then in the station

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -8,12 +8,13 @@ class DockingStation
   end
 
   def release_bike
-  	Bike.new
     raise "There are no more bikes!" if @docked_bikes.empty?
+    @docked_bikes.shift
   end
 
   def dock(new_bike)
-    @docked_bikes << new_bike
+      @docked_bikes << new_bike
+
   end
 
   def any_bikes_docked?
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,23 +2,23 @@ require 'docking_station'
 
 describe DockingStation do
 
-  it {is_expected.to respond_to (:release_bike)}
+  it {is_expected.to respond_to(:release_bike)}
 
   it 'expects bike to be working' do
-  	bike = subject.release_bike
+  	bike = Bike.new
   	expect(bike.working?).to eq true
   end
 
   it {is_expected.to respond_to(:dock).with(1).argument}
 
   it "expects bike to be docked" do
-    bike = subject.release_bike
+    bike = Bike.new
     subject.dock(bike)
     expect(subject.docked_bikes).to include(bike)
   end
 
   it "expects true if there is any bike in a docked_bike array" do
-    bike = subject.release_bike
+    bike = Bike.new
     subject.dock(bike)
     expect(subject.any_bikes_docked?).to eq true
   end
